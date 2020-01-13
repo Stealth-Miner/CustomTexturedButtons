97341e7073eb641c47baccf1d8f236c233313d0a
Stealth-Miner <59841670+Stealth-Miner@users.noreply.github.com>
2020-01-13 18:49:10 +0100

    Add files via upload
    Here is a exemple class that you can use for your button.

---
+++ GuiButtonSolo.java
@@ -0,0 +1,39 @@
+package kubyt.betaClasses;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.util.ResourceLocation;
+
+public class GuiButtonSolo extends GuiButton
+{
+	
+    protected static final ResourceLocation KubytbuttonTextures = new ResourceLocation("kubyt/gui/buttons.png");
+	
+    public GuiButtonSolo(int buttonID, int xPos, int yPos)
+    {
+        super(buttonID, xPos, yPos, 73, 20, "");
+    }
+
+    /**
+     * Draws this button to the screen.
+     */
+    public void drawButton(Minecraft mc, int mouseX, int mouseY)
+    {
+        if (this.visible)
+        {
+            mc.getTextureManager().bindTexture(KubytbuttonTextures);
+            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+            boolean flag = mouseX >= this.xPosition && mouseY >= this.yPosition && mouseX < this.xPosition + this.width && mouseY < this.yPosition + this.height;
+            // Y axis of image Location \|/
+            int i = 0;
+
+            if (flag)
+            {
+                i += this.height;
+            }
+
+            this.drawTexturedModalRect(this.xPosition, this.yPosition, 0, i, this.width, this.height);
+        }
+    }
+}
